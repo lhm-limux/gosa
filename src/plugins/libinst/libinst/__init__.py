43fb376829809d92267902ad2ab03047e300d721
Cajus Pollmeier <pollmeier@gonicus.de>
2011-03-30 10:51:16 +0200

    Changed structure

--- src/plugins/libinst/libinst/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
- This code is part of GOsa (http://www.gosa-project.org)
- Copyright (C) 2009, 2010 GONICUS GmbH
-
- ID: $$Id: __init__.py 487 2010-08-10 07:34:06Z cajus $$
-
- See LICENSE for more information about the licensing.
-"""
