7d5056d93b5e3a42d124eed76929a3178a037ebe
GOsa management agent on amqp <pollmeier@gonicus.de>
2011-04-14 11:38:09 +0200

    Fixed start for kde

--- contrib/notifications/send.sh
+++ contrib/notifications/send.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
 # Usage: send.sh user message
 
-# Collect potential addresses
-PIDS=$(pgrep -xfu $1 '(x-session-manager|.*ssh-agent.*)')
+# Get full path of notify-send
+NOTIFY=$(which notify-send)
 
-# Unique DBUS sessions for this user
-BAS=$(for P in $PIDS; do grep -z ^DBUS_SESSION_BUS_ADDRESS= /proc/$P/environ; done | sort | uniq)
+# Collect potential addresses for all sessions
+PIDS=$(pgrep -xfu $1 '(gnome-session|x-session-manager|/bin/sh /usr/bin/startkde)')
+BAS=$(for P in $PIDS; do
+    grep -z ^DBUS_SESSION_BUS_ADDRESS= /proc/$P/environ
+    done | sort | uniq)
 
 # Send notification
 for BA in $BAS; do
-    su "$1" -c "$BA notify-send 'Test message' '$2'"
+    su "$1" -c "$BA $NOTIFY 'Test message' '$2'"
 done
