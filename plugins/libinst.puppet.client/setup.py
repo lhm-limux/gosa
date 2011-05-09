be1a5190c07c7f66a6e4b4db3f71cec8befe18f1
GOsa management agent on amqp <pollmeier@gonicus.de>
2011-05-09 17:02:26 +0200

    Added new directory for client inst module

---
+++ plugins/libinst.puppet.client/setup.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+setup(
+    name = "libinst.puppet.client",
+    version = "1.0",
+    author = "Cajus Pollmeier",
+    author_email = "pollmeier@gonicus.de",
+    description = "Repository and installation abstraction library",
+    long_description = """
+This library handles the installation, configuration and repositories
+for various systems in your setup.
+""",
+    license = "LGPL",
+    url = "http://www.gosa-project.org",
+    classifiers = [
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: Console',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: System :: Systems Administration',
+        'Topic :: System :: Software Distribution',
+        'Topic :: System :: Monitoring',
+    ],
+
+    download_url = "http://oss.gonicus.de/pub/gosa",
+    packages = find_packages('src', exclude=['examples', 'tests']),
+    namespace_packages = ['libinst'],
+    package_dir={'': 'src'},
+
+    include_package_data = True,
+    package_data = {
+    },
+
+    test_suite = "nose.collector",
+    zip_safe = False,
+
+    setup_requires = ['nose', 'NoseXUnit', 'pylint'],
+    install_requires = [
+        'gosa.client',
+        'pyinotify',
+        'GitPython',
+        'PyYAML',
+    ],
+
+
+    entry_points = """
+        [gosa_client.modules]
+        gosa-client.puppet = libinst.puppet.client.main:PuppetClient
+    """
+)
