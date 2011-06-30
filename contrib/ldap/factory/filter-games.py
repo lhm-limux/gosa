6baaf450d6af43d6f8455c5c78a430fa43cac178
GOsa management agent on amqp <pollmeier@gonicus.de>
2011-06-30 17:30:16 +0200

    Updated filter-games

--- contrib/ldap/factory/filter-games.py
+++ contrib/ldap/factory/filter-games.py
@@ -1,1 +1,38 @@
 # -*- coding: utf-8 -*-
+
+def copyAttr(obj, dst, src, target):
+    return {target: getattr(obj, src)}
+
+def mkunixpasswd(obj, dst):
+    return {"userPassword": "{%s}%s" % (obj.PasswordMethod, obj.Password)}
+
+def mksmbpasswd(obj, dst):
+    return {"sambaLMPasswd": "asdfasdfasdfasdf:asdfasdfasdfasdf",
+            "sambaNTPasswd": "asdfdfghdfghdfgh:xcvbxcvbxcvbxcvb"}
+
+
+#---------------------------------------
+
+fltr1 = """
+dst.update(copyAttr(obj, dst, key, 'userPassword'))
+"""
+
+fltr2 = """
+dst.update(mkunixpasswd(obj, dst))
+dst.update(mksmbpasswd(obj, dst))
+"""
+
+#---------------------------------------
+
+
+
+
+class klass(object):
+    PasswordMethod = "MD5"
+    Password = "secret"
+
+obj = klass()
+key = "Password"
+dst = {}
+exec fltr1
+print dst
