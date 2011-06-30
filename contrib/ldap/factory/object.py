98b88c8a0d34e9fe01366d0861e9288488c8fde4
GOsa management agent on amqp <pollmeier@gonicus.de>
2011-06-30 15:10:12 +0200

    Updated schema and factory

--- contrib/ldap/factory/object.py
+++ contrib/ldap/factory/object.py
@@ -1,11 +1,26 @@
 # -*- coding: utf-8 -*-
 import os
+import time
+import datetime
 from lxml import etree, objectify
+
+# Map XML base types to python values
+TYPE_MAP = {
+        'Boolean': bool,
+        'String': unicode,
+        'Integer': int,
+        'Timestamp': time.time,
+        'Date': datetime.date,
+        'Binary': None,
+        'Dictionary': dict,
+        'List': list,
+        }
 
 
 class GOsaObjectFactory(object):
 
-    classes = {}
+    __xml_defs = {}
+    __classes = {}
 
     def __init__(self, path):
         # Initialize parser
@@ -31,7 +46,7 @@ class GOsaObjectFactory(object):
         # Load and validate objects
         try:
             xml = objectify.fromstring(open(path).read(), self.__parser)
-            self.classes[str(xml.Class['name'][0])] = xml
+            self.__xml_defs[str(xml.Class['name'][0])] = xml
 
         except etree.XMLSyntaxError as e:
             print "Error:", e
@@ -39,8 +54,12 @@ class GOsaObjectFactory(object):
 
     #@Command()
     def getObjectInstance(self, name, *args, **kwargs):
-        print "Initializing object of type %s" % name
+        if not name in self.__classes:
+            self.__classes[name] = self.__build_class(name)
 
+        return self.__classes[name](*args, **kwargs)
+
+    def __build_class(self, name):
         class klass(GOsaObject):
 
             def __init__(me, *args, **kwargs):
@@ -55,52 +74,85 @@ class GOsaObjectFactory(object):
         # Tweak name to the new target
         setattr(klass, '__name__', name)
 
-        # What kind of attributes do we have?
-        classr = self.classes[name].Class
+        # What kind of properties do we have?
+        classr = self.__xml_defs[name].Class
         props = {}
 
         try:
             for prop in classr['properties']['property']:
-                print "Property %s (%s)" % (prop['name'], prop['type'])
+                syntax = str(prop['syntax'])
                 props[str(prop['name'])] = {
                         'value': None,
-                        'type': str(prop['type'])}
+                        'type': TYPE_MAP[syntax],
+                        'syntax': syntax
+                        }
 
         except KeyError:
             pass
 
         setattr(klass, '__properties', props)
 
-        return klass(*args, **kwargs)
-
+        return klass
 
 
 class GOsaObject(object):
     # This may contain some useful stuff later on
 
     def __init__(self):
-        print "+++ superclass init"
+        pass
 
     def _setattr_(self, name, value):
         props = getattr(self, '__properties')
         if name in props:
-            props[name]['value'] = value
+
+            if props[name]['type']:
+
+                if issubclass(type(value), props[name]['type']):
+                    props[name]['value'] = value
+
+                else:
+                    raise TypeError("cannot assign value '%s'(%s) to property '%s'(%s)" % (
+                        value, type(value).__name__,
+                        name, props[name]['syntax']))
+
+            else:
+                props[name]['value'] = value
+
         else:
-            raise AttributeError("no such property")
+            raise AttributeError("no such property '%s'" % name)
 
     def _getattr_(self, name):
         props = getattr(self, '__properties')
+#        methods = getattr(self, '__methods')
+        methods = {'notify': {'ref': notify, 'signature': None}}
+
         if name in props:
             return props[name]['value']
+
+        elif name in methods:
+            return methods[name]['ref']
+
         else:
             raise AttributeError("no such property")
+
+    def commit(self):
+        print "--> built in commit method"
+
+    def delete(self):
+        print "--> built in delete method"
+
+
+def notify(message):
+    """ Dummy method to be dispatched """
+    print "Message:", message
 
 
 # --------------------------------- Test -------------------------------------
 f = GOsaObjectFactory('.')
 p = f.getObjectInstance('Person')
-
-p.sn = "Pollmeier"
-print p.sn
 print type(p)
-print p.__dict__
+
+p.sn = u"Pollmeier"
+print "sn:", p.sn
+p.commit()
+p.notify("Hallo Karl-Gustav!")
