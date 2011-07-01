5e36f32dbb42b5fa12f3ac007ac24379c5129277
Fabian Hickert <fabian.hickert@gonicus.de>
2011-07-01 11:47:38 +0200

    Added methods to GOsa Objects (xml)

--- contrib/ldap/factory/object.py
+++ contrib/ldap/factory/object.py
@@ -79,7 +79,8 @@ class GOsaObjectFactory(object):
 
         # What kind of properties do we have?
         classr = self.__xml_defs[name].Object
-        props = {}
+        props = {} 
+        methods = {}
 
         # Add documentation if available
         if 'description' in classr:
@@ -94,13 +95,33 @@ class GOsaObjectFactory(object):
                         'syntax': syntax
                         }
 
+            for method in classr['Functions']['Function']:
+                name = str(method['Name'])
+                def funk(*args, **kwargs):
+                    variables = {}  
+                    variables['title'] = args[0];
+                    variables['message'] = args[1];
+                    self.__exec(unicode(str(method['Code']).strip()), variables)
+
+                methods[name] = {
+                        'ref': funk
+                        }
+
         except KeyError:
             pass
 
         setattr(klass, '__properties', props)
-        setattr(klass, '__methods', {})
+        setattr(klass, '__methods', methods)
 
         return klass
+
+    def __exec(self, code, args):
+        print args 
+
+        for _key in args.keys():
+            exec "%(key)s = args['%(key)s']" % {'key': _key}
+        print message
+        exec code
 
 
 class GOsaObject(object):
@@ -152,9 +173,9 @@ class GOsaObject(object):
         print "--> built in delete method"
 
 
-def notify(message):
+def notify(title, message):
     """ Dummy method to be dispatched """
-    print "Message:", message
+    print "Message:", title, message
 
 
 # --------------------------------- Test -------------------------------------
@@ -165,4 +186,4 @@ print "Object type:", type(p)
 p.sn = u"Pollmeier"
 print "sn:", p.sn
 p.commit()
-#p.notify("Hallo Karl-Gustav!")
+p.notify("Hallo", "Hallo Karl-Gustav!")
