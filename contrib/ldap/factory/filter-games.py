e4df8b0ef60c1adbb5283be90e43bb0e21ac7d89
GOsa management agent on amqp <pollmeier@gonicus.de>
2011-07-04 16:14:48 +0200

    Updated filter playground

--- contrib/ldap/factory/filter-games.py
+++ contrib/ldap/factory/filter-games.py
@@ -1,57 +1,93 @@
 # -*- coding: utf-8 -*-
-from backend.registry import loadAttr, ObjectBackendRegistry
+import datetime
+from time import mktime
 
-####
-#### OUT Test
-####
-#
-#def copyAttr(obj, dst, src, target):
-#    return {target: getattr(obj, src)}
-#
-#def mkunixpasswd(obj, dst):
-#    return {"userPassword": "{%s}%s" % (obj.PasswordMethod, obj.Password)}
-#
-#def mksmbpasswd(obj, dst):
-#    return {"sambaLMPasswd": "asdfasdfasdfasdf:asdfasdfasdfasdf",
-#            "sambaNTPasswd": "asdfdfghdfghdfgh:xcvbxcvbxcvbxcvb"}
-#
-#fltr1 = """
-#dst.update(copyAttr(obj, dst, key, 'userPassword'))
-#"""
-#
-#fltr2 = """
-#dst.update(mkunixpasswd(obj, dst))
-#dst.update(mksmbpasswd(obj, dst))
-#"""
+class ElementFilter(object):
+
+    def __init__(self):
+        #TODO: load parameters
+        #      decide what we "need"
+        pass
+
+    def process(self, obj, key, value):
+        raise NotImplementedError("not implemented")
+
+    def getReferences(self):
+        """
+        Return the properties we reference to. This makes us speed up
+        value loading, because we cann pull everything we need in one step.
+        """
+        return {}
 
 
-###
-### IN Test
-###
-fltr3 = """
-dst = loadAttr(obj, "givenName")[0]
-"""
+class ToUnixTime(ElementFilter):
 
-class klass(object):
-    _backend = "ldap"
-    PasswordMethod = "MD5"
-    Password = "secret"
+    def __init__(self):
+        super(ToUnixTime, self).__init__()
 
-    def load(self, dn):
-        self.reg = ObjectBackendRegistry.getInstance()
-        self.uuid = self.reg.dn2uuid(self._backend, dn)
-
-obj = klass()
-key = "GivenName"
-dst = {}
-#exec fltr2
-#print dst
+    def process(self, obj, key, value):
+        value = mktime(value.timetuple())
+        return key, int(value)
 
 
-###
-### IN
-###
+class FromUnixTime(ElementFilter):
 
-obj.load("cn=Cajus Pollmeier,ou=people,ou=Technik,dc=gonicus,dc=de")
-exec fltr3
-print dst
+    def __init__(self):
+        super(FromUnixTime, self).__init__()
+
+    def process(self, obj, key, value):
+        value = datetime.datetime.fromtimestamp(value)
+        return key, value
+
+
+
+class Target(ElementFilter):
+
+    def __init__(self):
+        super(Target, self).__init__()
+
+    def process(self, obj, key, value, new_key):
+        key = new_key
+        return key, value
+
+
+class Load(ElementFilter):
+
+    def __init__(self):
+        super(Load, self).__init__()
+
+    def process(self, obj, key, value, attr):
+        return key, 854711
+
+
+# --- Out test
+
+class dummy(object):
+
+    cn = "Cajus Pollmeier"
+    givenName = "Cajus"
+    sn = "Pollmeier"
+    uid = "cajus"
+    tm = datetime.datetime.now()
+
+o = dummy()
+t = ToUnixTime()
+f = FromUnixTime()
+s = Target()
+l = Load()
+
+key = "tm"
+value = o.tm
+key, value = t.process(o, key, value)
+key, value = s.process(o, key, value, "sambaLogoffTime")
+
+print "%s=%s" % (key, value)
+
+
+# --- In test
+
+key = "tm"
+value = None
+key, value = l.process(o, key, value, "sambaLogoffTime")
+key, value = f.process(o, key, value)
+print "%s=%s" % (key, str(value))
