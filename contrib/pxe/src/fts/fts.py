941b4925c0c753257df6ec5d535163123e838980
GOsa management agent on amqp-homer <jan.wenzel@gonicus.de>
2011-08-15 17:19:38 +0200

    Change class name to PxeFS. Add static directory functions.

--- contrib/pxe/src/fts/fts.py
+++ contrib/pxe/src/fts/fts.py
@@ -9,7 +9,9 @@
  See LICENSE for more information about the licensing.
 """
 
+from gosa.common import Environment
 from gosa.common.components import AMQPServiceProxy
+
 from time import time
 
 import errno
@@ -35,6 +37,8 @@ testclients = {
     "de:ad:d9:57:56:d5": "install"
 }
 
+static=os.getcwd()+'/pxelinux.static'
+
 # Create connection to service
 #try:
 #    proxy = AMQPServiceProxy('amqps://ldapadmin:secret@amqp.intranet.gonicus.de/org.gosa')
@@ -50,13 +54,6 @@ def getDepth(path):
         return 0
     else:
         return path.count('/')
-
-def dirFromList(list):
-    """
-    Return a properly formatted list of items suitable to a directory listing.
-    [['a', 'b', 'c']] => [[('a', 0), ('b', 0), ('c', 0)]]
-    """
-    return [[(x, 0) for x in list]]
 
 def getParts(path):
     """
@@ -80,10 +77,10 @@ class FileStat(fuse.Stat):
         self.st_mtime = self.st_atime
         self.st_ctime = self.st_atime
 
-class FTS(Fuse):
+class PxeFS(Fuse):
     def __init__(self, *args, **kw):
         Fuse.__init__(self, *args, **kw)
-        self.root = "/"
+        self.root = os.sep
         self.filesystem = {
             self.root : {
                 "content": "",
@@ -91,12 +88,12 @@ class FTS(Fuse):
         }
 
         for testclient in testclients.keys():
-            self.filesystem[self.root]= { testclient : { "content" : u'''vga=normal initrd=debian-installer/i386/initrd.gz
+            self.filesystem[self.root]= { testclient : { "content" : r'''vga=normal initrd=debian-installer/i386/initrd.gz
 netcfg/choose_interface=eth0 locale=de_DE debian-installer/country=DE
 debian-installer/language=de
 debian-installer/keymap=de-latin1-nodeadkeys
 console-keymaps-at/keymap=de-latin1-nodeadkeys auto-install/enable=false
-preseed/url=https://amqp.intranet.gonicus.de:8080/preseed/de-ad-d9-57-56-d5 debian/priority=critical hostname=dyn-10 domain=please-fixme.org DEBCONF_DEBUG=5 svc_key=f1p8zRBGrUA26Nn+2qBS/JC8KOXHTEfgIEq5Le2WC4jW2xUuVzzHnO9LYiH8hYLNXHo7V9+2Aiz8\\n/XU6xxcusWUiMjXgdZcDe8wJtXR5krg=\\n''' } }
+preseed/url=https://amqp.intranet.gonicus.de:8080/preseed/de-ad-d9-57-56-d5 debian/priority=critical hostname=dyn-10 domain=please-fixme.org DEBCONF_DEBUG=5 svc_key=f1p8zRBGrUA26Nn+2qBS/JC8KOXHTEfgIEq5Le2WC4jW2xUuVzzHnO9LYiH8hYLNXHo7V9+2Aiz8\n/XU6xxcusWUiMjXgdZcDe8wJtXR5krg=\n''' } }
 
     def getattr(self, path):
         result = FileStat()
@@ -105,23 +102,19 @@ preseed/url=https://amqp.intranet.gonicus.de:8080/preseed/de-ad-d9-57-56-d5 debi
         else:
             result.st_mode = stat.S_IFREG | 0666
             result.st_nlink = 1
-            result.st_size = len(self.getContent(path))
+            result.st_size = self.getSize(path)
         return result
 
     def read(self, path, size, offset):
-        #return self.getContent(path)[offset:offset+size]
-        result = self.getContent(path)
-        print result
-        return result
+        return self.getContent(path, size, offset)
 
     def flush(self, filehandle=None):
         return 0
 
     def readdir(self, path, offset):
         direntries=[ '.', '..' ]
-        print "Path:", path
-        print "Offset:", offset
-        print self.filesystem[path].keys()
+        if os.path.exists(os.sep.join((static, path))):
+            direntries.extend(os.listdir(os.sep.join((static, path))))
         if self.filesystem[path].keys():
             direntries.extend(self.filesystem[path].keys())
         #else:
@@ -131,17 +124,32 @@ preseed/url=https://amqp.intranet.gonicus.de:8080/preseed/de-ad-d9-57-56-d5 debi
         for directory in direntries:
             yield fuse.Direntry(directory)
 
-    def getContent(self, path):
+    def getContent(self, path, size, offset):
         result = ""
-        print "Path:", path
         if getDepth(path) > 1:
             print "Depth for path %s: %i " % (path, getDepth(path))
             pass
         else:
-            path = path.lstrip("/")
-            if path in self.filesystem[self.root].keys():
-                result = str(self.filesystem[self.root][path]["content"])
+            if os.path.exists(os.sep.join((static, path))):
+                with open(os.sep.join((static, path))) as f:
+                    f.seek(offset)
+                    result = f.read(size)
+            elif path.lstrip(os.sep) in self.filesystem[self.root].keys():
+                result = str(self.filesystem[self.root][path.lstrip(os.sep)]["content"])[offset:offset+size]
         return result
+
+    def getSize(self, path):
+        result = 0
+        if getDepth(path) > 1:
+            print "Depth for path %s: %i " % (path, getDepth(path))
+            pass
+        else:
+            if os.path.exists(os.sep.join((static, path))):
+                result = os.path.getsize(os.sep.join((static, path)))
+            elif path.lstrip(os.sep) in self.filesystem[self.root].keys():
+                result = len(str(self.filesystem[self.root][path.lstrip(os.sep)]["content"]))
+        return result
+
 
 def main():
     usage = """
@@ -149,16 +157,14 @@ Userspace tftp supplicant: create pxelinux configuration files based on external
 
 """ + Fuse.fusage
 
-    server = FTS(version="%prog " + fuse.__version__,
+    server = PxeFS(version="%prog " + fuse.__version__,
                  usage=usage,
                  dash_s_do='setsingle')
 
     # Disable multithreading: if you want to use it, protect all method of
     # XmlFile class with locks, in order to prevent race conditions
     server.multithreaded = False
-
-    server.parser.add_option(mountopt="root", metavar="PATH", default='/',
-                             help="mirror filesystem from under PATH [default: %default]")
+    server.parser.add_option(mountopt="root", metavar="PATH", default='/', help="mirror filesystem from under PATH [default: %default]")
     server.parse(values=server, errex=1)
 
     try:
