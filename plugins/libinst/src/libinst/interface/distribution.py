f7f627aa342ecbb8c1d387c5fc842f18dc96a73c
GOsa management agent on amqp-homer <jan.wenzel@gonicus.de>
2011-08-26 12:57:19 +0200

    Update API documentation.

--- plugins/libinst/src/libinst/interface/distribution.py
+++ plugins/libinst/src/libinst/interface/distribution.py
@@ -19,42 +19,85 @@ class DistributionHandler(object):
 
     def createDistribution(self, session, name, mirror=None):
         """
-        TODO
+        Create a new distribution with a specific name.
+        =============== ==================================
+        Parameter       Description
+        =============== ==================================
+        session         The sqlalchemy session to use.
+        name            The name of the new distribution (i.e. debian)
+        mirror          Specifies an optional mirror origin, distribution will be unmanaged
+        =============== ==================================
         """
         raise NotImplementedError("%s is not implemented" % stack()[0][3])
 
-    def removeDistribution(self, session, name):
+    def removeDistribution(self, session, name, recursive=False):
         """
-        TODO
-        """
-        raise NotImplementedError("%s is not implemented" % stack()[0][3])
-
-    def createRelease(self, session, distribution, name, parent=None):
-        """
-        TODO
-        """
-        raise NotImplementedError("%s is not implemented" % stack()[0][3])
-
-    def removeRelease(self, session, name):
-        """
-        TODO
+        Removes specified distribution (must not have any releases), removal of releases has to be forced.
+        =============== ==================================
+        Parameter       Description
+        =============== ==================================
+        session         The sqlalchemy session to use.
+        name            The name of the distribution to be removed.
+        recursive       Choose whether to force removal of all releases depending on this distribution.
         """
         raise NotImplementedError("%s is not implemented" % stack()[0][3])
 
-    def renameRelease(self, session, source_name, target_name):
+    def createRelease(self, session, distribution, name):
         """
-        TODO
+        Creates a new release in the given distribution.
+        =============== ==================================
+        Parameter       Description
+        =============== ==================================
+        session         The sqlalchemy session to use.
+        distribution    The name of the distribution this release belongs to.
+        name            The name for the new release, parent releases are prepended with slashes (i.e. "lenny/production")
         """
         raise NotImplementedError("%s is not implemented" % stack()[0][3])
 
-    def addPackage(self, session, url, release=None):
+    def removeRelease(self, session, release, recursive=False):
         """
-        TODO
+        Removes specified release (removal of child releases needs to be forced).
+        =============== ==================================
+        Parameter       Description
+        =============== ==================================
+        session         The sqlalchemy session to use.
+        release         The name of the release to remove.
+        recursive       Choose whether method should remove child releases.
         """
         raise NotImplementedError("%s is not implemented" % stack()[0][3])
 
-    def _updateInventory(self, session, distribution=None, release=None):
+    def renameRelease(self, session, source, target):
         """
-        TODO
+        Renames specified release.
+        =============== ==================================
+        Parameter       Description
+        =============== ==================================
+        session         The sqlalchemy session to use.
+        source          The name of the release that should be renamed.
+        target          The new name for the release
+
         """
         raise NotImplementedError("%s is not implemented" % stack()[0][3])
+
+    def addPackage(self, session, url, distribution=None, release=None):
+        """
+        Adds a local or remote specified package to the given distribution or release.
+        =============== ==================================
+        Parameter       Description
+        =============== ==================================
+        session         The sqlalchemy session to use.
+        url             URL of package, can be a local or remote path (supported are ftp/http/https URLs).
+        distribution    The name of the distribution to which this package should be added (adds the package to all releases of the distribution)
+        release         The name of the release to which this package should be added.
+        """
+        raise NotImplementedError("%s is not implemented" % stack()[0][3])
+
+    def _updateInventory(self, session, release=None, distribution=None):
+        """
+        Updates package indexes for the given release or distribution.
+        =============== ==================================
+        Parameter       Description
+        =============== ==================================
+        session         The sqlalchemy session to use.
+        release         Update package index for specific release (including child releases).
+        distribution    The name of the distribution which should be updated (all releases including child releases).
