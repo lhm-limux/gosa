609dc955eb2f85603417f99d49e277362206b50e
GOsa management agent on amqp <pollmeier@gonicus.de>
2011-09-06 17:39:57 +0200

    Updated scheduler stuff

--- gosa.agent/src/gosa/agent/scheduler.py
+++ gosa.agent/src/gosa/agent/scheduler.py
@@ -5,6 +5,7 @@ TODO: docs
 from zope.interface import implements
 from gosa.common.handler import IInterfaceHandler
 from gosa.common import Environment
+from gosa.common.utils import N_
 from gosa.common.components.scheduler import Scheduler, set_job_property
 from gosa.common.components.scheduler.jobstores.sqlalchemy_store import SQLAlchemyJobStore
 
@@ -37,9 +38,11 @@ class SchedulerService(object):
         # Start migration job
         self.sched.add_interval_job(self.migrate, seconds=60)
 
+        #TODO: add event listener for remote schedulers
+
     def stop(self):
         """ Stop scheduler service. """
-        sched.shutdown()
+        self.sched.shutdown()
 
     def migrate(self):
         """
@@ -47,3 +50,52 @@ class SchedulerService(object):
         """
         self.env.log.debug("scheduler: looking for stale jobs")
         #TODO: realy look for stale jobs
+        #self.sched.reschedule()
+
+    @Command(__help__=N_("Return scheduler information for a specific job."))
+    def schedulerGetJob(self, job_id):
+        return self.sched.get_job_by_id(job_id)
+
+    @Command(__help__=N_("List jobs matchings an optional filter."))
+    def schedulerGetJobs(self, fltr=None):
+        res = {}
+
+        for job in self.sched.get_jobs():
+            res[job.uuid] = []
+
+        return res
+
+    @Command(needsUser=True, __help__=N_("Add a new job to the scheduler."))
+    def schedulerAddJob(self, user):
+    #def add_job(self, trigger, func, args, kwargs, jobstore='default',
+    #            **options):
+    #    """
+    #    Adds the given job to the job list and notifies the scheduler thread.
+
+    #    :param trigger: alias of the job store to store the job in
+    #    :param func: callable to run at the given time
+    #    :param args: list of positional arguments to call func with
+    #    :param kwargs: dict of keyword arguments to call func with
+    #    :param jobstore: alias of the job store to store the job in
+    #    :rtype: :class:`~gosa.common.components.scheduler.job.Job`
+    #    """
+    # Options:
+    # * misfire_grace_time
+    # * coalesce
+    # * name
+    # * max_runs
+    # * max_instances
+    # * tag
+    # * owner
+    # * description
+    # * callback
+        #return job.uuid
+        pass
+
+    @Command(needsUser=True, __help__=N_("Add a new job to the scheduler."))
+    def schedulerUpdateJob(self, user, job_id):
+        pass
+
+    @Command(needsUser=True, __help__=N_("Remove job from the scheduler."))
+    def schedulerRemoveJob(self, user, job_id):
+        pass
