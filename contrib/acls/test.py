bfe1d8786c769aeb789e1795c9d623d343f76955
Fabian Hickert <fabian.hickert@gonicus.de>
2011-09-08 15:47:18 +0200

    Fixed saving of scope values, do not save integer values

--- contrib/acls/test.py
+++ contrib/acls/test.py
@@ -1,28 +1,28 @@
 from acl import Acl, AclSet, AclResolver
 import os
 
-#if not os.path.exists("agent.acl"):
-#    acl1 = Acl(Acl.SUB)
-#    acl1.add_members([u'cajus', u'hickert'])
-#    acl1.add_action(u'gosa.*.cancelEvent', ['r','w','x'], {})
-#    aclSet1 = AclSet(u"dc=gonicus,dc=de")
-#    aclSet1.add(acl1)
-#
-#    acl2 = Acl(Acl.RESET)
-#    acl2.add_members([u'hickert'])
-#    acl2.add_action(u'gosa.scheduler.cancelEvent', ['r','w','x'], {'owner': 'hickert'})
-#    acl3 = Acl(Acl.SUB)
-#    acl3.add_members([u'cajus'])
-#    acl3.add_action(u'gosa.scheduler.cancelEvent', ['r','w','x'], {})
-#    aclSet2 = AclSet(u"ou=technik,dc=intranet,dc=gonicus,dc=de")
-#    aclSet2.add(acl2)
-#    aclSet2.add(acl3)
-#
-#    resolver = AclResolver.get_instance()
-#    resolver.add_acl_set(aclSet1)
-#    resolver.add_acl_set(aclSet2)
-#
-#    resolver.save_to_file()
+if not os.path.exists("agent.acl"):
+    acl1 = Acl(Acl.SUB)
+    acl1.add_members([u'cajus', u'hickert'])
+    acl1.add_action(u'gosa.*.cancelEvent', 'rwx', {})
+    aclSet1 = AclSet(u"dc=gonicus,dc=de")
+    aclSet1.add(acl1)
+
+    acl2 = Acl(Acl.RESET)
+    acl2.add_members([u'hickert'])
+    acl2.add_action(u'gosa.scheduler.cancelEvent', 'rwx', {'owner': 'hickert'})
+    acl3 = Acl(Acl.SUB)
+    acl3.add_members([u'cajus'])
+    acl3.add_action(u'gosa.scheduler.cancelEvent', 'rwx', {})
+    aclSet2 = AclSet(u"ou=technik,dc=intranet,dc=gonicus,dc=de")
+    aclSet2.add(acl2)
+    aclSet2.add(acl3)
+
+    resolver = AclResolver.get_instance()
+    resolver.add_acl_set(aclSet1)
+    resolver.add_acl_set(aclSet2)
+
+    resolver.save_to_file()
 
 # Load definition from file
 resolver = AclResolver.get_instance()
@@ -34,7 +34,7 @@ deps = ['ou=1,ou=technik,dc=intranet,dc=gonicus,dc=de',
 
 actions = ["gosa.scheduler.cancelEvent"]
 options = {'owner': 'hickert'}
-acls = ['r','w']
+acls = 'rw'
 
 for dep in deps:
     for user in ('cajus','hickert'):
@@ -48,3 +48,5 @@ for dep in deps:
                 print "|---> %s darf NICHT %s in %s" % (user, action, dep)
                 print ""
                 pass
+
+resolver.save_to_file()
