4e698b589224f784ad04187862a7ba2404cba288
Fabian Hickert <fabian.hickert@gonicus.de>
2011-09-08 15:35:47 +0200

    Added agent.acl

--- contrib/acls/test.py
+++ contrib/acls/test.py
@@ -1,30 +1,28 @@
 from acl import Acl, AclSet, AclResolver
 import os
 
-fname = "agent.acl"
-
-if not os.path.exists(fname):
-    acl1 = Acl(Acl.SUB)
-    acl1.add_members([u'cajus', u'hickert'])
-    acl1.add_action(u'gosa.*.cancelEvent', ['r','w','x'], {})
-    aclSet1 = AclSet(u"dc=gonicus,dc=de")
-    aclSet1.add(acl1)
-
-    acl2 = Acl(Acl.RESET)
-    acl2.add_members([u'hickert'])
-    acl2.add_action(u'gosa.scheduler.cancelEvent', ['r','w','x'], {'owner': 'hickert'})
-    acl3 = Acl(Acl.SUB)
-    acl3.add_members([u'cajus'])
-    acl3.add_action(u'gosa.scheduler.cancelEvent', ['r','w','x'], {})
-    aclSet2 = AclSet(u"ou=technik,dc=intranet,dc=gonicus,dc=de")
-    aclSet2.add(acl2)
-    aclSet2.add(acl3)
-
-    resolver = AclResolver.get_instance()
-    resolver.add_acl_set(aclSet1)
-    resolver.add_acl_set(aclSet2)
-
-    resolver.save_to_file()
+#if not os.path.exists("agent.acl"):
+#    acl1 = Acl(Acl.SUB)
+#    acl1.add_members([u'cajus', u'hickert'])
+#    acl1.add_action(u'gosa.*.cancelEvent', ['r','w','x'], {})
+#    aclSet1 = AclSet(u"dc=gonicus,dc=de")
+#    aclSet1.add(acl1)
+#
+#    acl2 = Acl(Acl.RESET)
+#    acl2.add_members([u'hickert'])
+#    acl2.add_action(u'gosa.scheduler.cancelEvent', ['r','w','x'], {'owner': 'hickert'})
+#    acl3 = Acl(Acl.SUB)
+#    acl3.add_members([u'cajus'])
+#    acl3.add_action(u'gosa.scheduler.cancelEvent', ['r','w','x'], {})
+#    aclSet2 = AclSet(u"ou=technik,dc=intranet,dc=gonicus,dc=de")
+#    aclSet2.add(acl2)
+#    aclSet2.add(acl3)
+#
+#    resolver = AclResolver.get_instance()
+#    resolver.add_acl_set(aclSet1)
+#    resolver.add_acl_set(aclSet2)
+#
+#    resolver.save_to_file()
 
 # Load definition from file
 resolver = AclResolver.get_instance()
