0237adaf8efe98b81f589f14316a15f170abadc3
Fabian Hickert <fabian.hickert@gonicus.de>
2011-09-08 10:00:29 +0200

    Updated ACL class

--- contrib/acls/test.py
+++ contrib/acls/test.py
@@ -1,4 +1,5 @@
 import re
+import ldap
 
 
 #TODO: The permission types (rwcdm) are not checked yet.
@@ -6,6 +7,7 @@ import re
 #TODO: Think about ldap relations, how to store and load objects.
 #TODO: Groups are not supported yet
 #TODO: What about object groups, to be able to inlcude clients?
+#TODO: Use ldap dn split
 
 
 
@@ -15,7 +17,7 @@ This is a collection of classes that can manager Access control lists.
 AclSet      - Is a container class for Acl objects. An AclSet-object can be attached
               to ldap organizationalUnits to restrict permissions for a set of
               users.
-Acl         - This class represent a single ACL rule, these Acl objects can be
+Acl         - This class represents a single ACL rule, Acl-objects can be
               bundled in an AclSet object.
 
 AclResoler  - This class is used to manage all Acl- and AclSets objects.
@@ -32,6 +34,9 @@ class AclSet(list):
         self.location = location
 
     def add(self, item):
+        """
+        Adds a new acl object to this aclSet.
+        """
         if not isinstance(item, Acl):
             raise TypeError, 'item is not of type %s' % Acl
 
@@ -45,7 +50,11 @@ class AclSet(list):
 
 
 class Acl(object):
-
+    """
+    The Acl class contains list of action for a set of members.
+    These Acl classes can then be bundled and attached to a ldap base using
+    the aclSet class.
+    """
     priority = None
 
     ONE = 1
@@ -68,13 +77,17 @@ class Acl(object):
         self.acl_type = acl_type
 
     def addMember(self, member):
-
+        """
+        Adds a new member to this acl.
+        """
         if type(member) != str:
             raise(Exception("Member should be of type str!"))
         self.members.append(member)
 
     def addMembers(self, members):
-
+        """
+        Adds a list of new members to this acl.
+        """
         if type(members) != list or len(members) == 0:
             raise(Exception("Requires a list of members!"))
 
@@ -82,15 +95,25 @@ class Acl(object):
             self.addMember(member)
 
     def addAction(self, action, acls, options):
+        """
+        Adds a new action to this acl.
+        """
         acl = { 'action': action,
                 'acls': acls,
                 'options': options}
         self.actions.append(acl)
 
     def getMembers(self):
+        """
+        Returns the list of members this ACL is valid for.
+        """
         return(self.member)
 
-    def match(self, member, action, options):
+    def match(self, member, action, acls, options = {}):
+        """
+        Check of the requested user, action and the action options match this
+        acl-object.
+        """
         allowd = False
 
         if member in self.members:
@@ -101,42 +124,80 @@ class Acl(object):
                 test_act = re.sub(r'(^|\\.)(\\\*)(\\.|$)', '\\1.*\\3', test_act)
                 test_act = re.sub(r'(^|\\.)(\\#)(\\.|$)', '\\1[^\.]*\\3', test_act)
 
-                # Check if the action string matches the acl-action definition
-                if re.match(test_act, action):
-                    return True
+                # Check if the requested-action matches the acl-action.
+                if not re.match(test_act, action):
+                    continue
+
+                # Check if the required permissions are allowed.
+                if (set(acls) & set(act['acls'])) != set(acls):
+                    continue
+
+                # Check if all required options are given
+                for entry in act['options']:
+                    if entry not in options:
+                        print "Option '%s' is missing" % entry
+                        continue
+
+                    if act['options'][entry] != options[entry]:
+                        print "Option '%s:%s' does not match" % (entry,
+                                act['options'][entry])
+                        continue
+
+                return(True)
+
 
         return False
 
     def getType(self):
+        """
+        Returns the type of an ACL.
+        SUB, PSUB, RESET, ...
+        """
         return(self.acl_type)
 
 
 class AclResolver(object):
 
     aclSets = []
+    ldapBase = ""
 
-    def __init__(self):
-        self.refreshAcls()
+    def __init__(self, ldapBase):
+        self.ldapBase = ldapBase;
 
     def addAclSet(self, acl):
+        """
+        Adds an aclSet object to the list of active-acl rules.
+        """
         self.aclSets.append(acl)
 
     def refreshAcls(self):
+        """
+        Re-reads the permission settings from the ldap server.
+        """
         pass
 
-    def getPermissions(self, member, location, action, options= {}):
+    def getPermissions(self, member, location, action, acls, options= {}):
+        """
+        Check permissions for a given user(member) and a location.
+        """
 
         # Collect all acls matching the where statement
         allowed = False
         reset = False
-        while "," in location:
+
+        # Remove the first part of the dn, until we reach the ldap base.
+        while self.ldapBase in location:
+
+            # Check acls for each acl set.
             for aclSet in self.aclSets:
 
+                # Skip acls that do not match the current ldap location.
                 if location != aclSet.location:
                     continue
 
+                # Resolve ACLs.
                 for acl in aclSet:
-                    if acl.match(member, action, options):
+                    if acl.match(member, action, acls, options):
                         if acl.getType() == Acl.RESET:
                             reset = True
                         elif acl.getType() == Acl.PSUB:
@@ -144,8 +205,8 @@ class AclResolver(object):
                         elif acl.getType() in (Acl.SUB,) and not reset:
                             allowed = True
 
-            #TODO Use ldap dn split
-            location = re.sub("^[^,]+,","",location)
+            # Remove the first part of the dn
+            location =  ','.join(ldap.dn.explode_dn(location)[1::])
 
         return(allowed)
 
@@ -158,7 +219,7 @@ aclSet1.add(acl1)
 
 acl2 = Acl(Acl.RESET)
 acl2.addMembers(['hickert'])
-acl2.addAction('gosa.scheduler.cancelEvent', ['r','w','x'], {})
+acl2.addAction('gosa.scheduler.cancelEvent', ['r','w','x'], {'owner': 'hickert'})
 acl3 = Acl(Acl.SUB)
 acl3.addMembers(['cajus'])
 acl3.addAction('gosa.scheduler.cancelEvent', ['r','w','x'], {})
@@ -166,7 +227,7 @@ aclSet2 = AclSet("ou=technik,dc=intranet,dc=gonicus,dc=de")
 aclSet2.add(acl2)
 aclSet2.add(acl3)
 
-resolver = AclResolver()
+resolver = AclResolver("dc=gonicus,dc=de")
 resolver.addAclSet(aclSet1)
 resolver.addAclSet(aclSet2)
 
@@ -183,11 +244,13 @@ for dep in deps:
     print dep
     for user in ('cajus','hickert'):
 
+        options = {'owner': 'hickert'}
+        acls = ['r','w']
         for action in actions:
-            if(resolver.getPermissions(user,dep,action)):
-                print " >%s darf %s" % (user,action)
+            if(resolver.getPermissions(user, dep, action, acls, options)):
+                print "   >%s darf %s" % (user, action)
                 pass
             else:
-                print " >%s darf %s nicht!!!" % (user,action)
+                print "   >%s darf %s nicht!!!" % (user,action)
                 pass
 
