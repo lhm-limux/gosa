cad599d9165942a5acc3befbb1d03e3ce7a326a7
Fabian Hickert <fabian.hickert@gonicus.de>
2011-09-13 10:45:31 +0200

    Added Acl tests for scope sub

--- gosa.agent/src/tests/acl_tests.py
+++ gosa.agent/src/tests/acl_tests.py
@@ -18,20 +18,60 @@ class TestACLResolver(unittest.TestCase):
         self.resolver = ACLResolver()
         self.ldap_base = self.resolver.base
 
+    def test_simple_acls_scope_sub(self):
+
+        self.resolver.clear()
+        base = "dc=a," + self.ldap_base
+
+        # Create simple acls with scope SUB
+        aclset = ACLSet(base)
+        acl = ACL(scope=ACL.SUB)
+        acl.add_members([u'tester1'])
+        acl.add_action('com.gosa.factory','rwx')
+        aclset.add(acl)
+        self.resolver.add_acl_set(aclset)
+
+        # Check for read, write, create, execute permisions
+        base = "dc=a," + self.ldap_base
+        self.assertTrue(self.resolver.get_permissions('tester1',base,'com.gosa.factory','r'),
+                "User is not able to read!")
+        self.assertTrue(self.resolver.get_permissions('tester1',base,'com.gosa.factory','w'),
+                "User is not able to write!")
+        self.assertTrue(self.resolver.get_permissions('tester1',base,'com.gosa.factory','x'),
+                "User is not able to execute!")
+        self.assertFalse(self.resolver.get_permissions('tester1',base,'com.gosa.factory','d'),
+                "User is able to delete, this acl was not defined before!")
+
+        # Check for permissions one level above the location we've created acls for.
+        # This should return True.
+        base = "dc=b,dc=a," + self.ldap_base
+        self.assertTrue(self.resolver.get_permissions('tester1',base,'com.gosa.factory','r'),
+                "The user is not able to read, this is wrong!")
+
+        # Check for permissions tow levels above the location we've created acls for.
+        # This should return True too.
+        base = "dc=c,dc=b,dc=a," + self.ldap_base
+        self.assertTrue(self.resolver.get_permissions('tester1',base,'com.gosa.factory','r'),
+                "The user is not able to read, this is wrong!")
+
+        # Check for permissions one level below the location we've created acls for.
+        # This should return False.
+        base = self.ldap_base
+        self.assertFalse(self.resolver.get_permissions('tester1',base,'com.gosa.factory','r'),
+                "The user is able to read, this is wrong!")
+
 
     def test_simple_acls_scope_one(self):
 
         self.resolver.clear()
-
         base = "dc=a," + self.ldap_base
 
         # Create simple acls with scope ONE
         aclset = ACLSet(base)
         acl = ACL(scope=ACL.ONE)
-        acl.add_members([u'tester1', u'tester2'])
+        acl.add_members([u'tester1'])
         acl.add_action('com.gosa.factory','rwx')
         aclset.add(acl)
-
         self.resolver.add_acl_set(aclset)
 
         # Check for read, write, create, execute permisions
@@ -49,19 +89,13 @@ class TestACLResolver(unittest.TestCase):
         base = "dc=b,dc=a," + self.ldap_base
         self.assertFalse(self.resolver.get_permissions('tester1',base,'com.gosa.factory','r'),
                 "The user is able to read, this is wrong!")
-        self.assertFalse(self.resolver.get_permissions('tester1',base,'com.gosa.factory','w'),
-                "The user is able to write, this is wrong!")
-        self.assertFalse(self.resolver.get_permissions('tester1',base,'com.gosa.factory','x'),
-                "The user is able to execute, this is wrong!")
 
         # Check for permissions one level below the location we've created acls for.
         base = self.ldap_base
         self.assertFalse(self.resolver.get_permissions('tester1',base,'com.gosa.factory','r'),
                 "The user is able to read, this is wrong!")
-        self.assertFalse(self.resolver.get_permissions('tester1',base,'com.gosa.factory','w'),
-                "The user is able to write, this is wrong!")
-        self.assertFalse(self.resolver.get_permissions('tester1',base,'com.gosa.factory','x'),
-                "The user is able to execute, this is wrong!")
+
+
 
 if __name__ == '__main__':
     unittest.main()
