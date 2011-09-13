41250302aa3347390dc36831ba47a73ee67f7bc6
Fabian Hickert <fabian.hickert@gonicus.de>
2011-09-13 10:10:54 +0200

    Added some more checks

--- gosa.agent/src/tests/acl_tests.py
+++ gosa.agent/src/tests/acl_tests.py
@@ -19,10 +19,11 @@ class TestACLResolver(unittest.TestCase):
         self.ldap_base = self.resolver.base
 
 
-    def test_simple_acls(self):
+    def test_simple_acls_scope_one(self):
+
         self.resolver.clear()
 
-        base = self.ldap_base
+        base = "dc=a," + self.ldap_base
 
         # Create simple acls with scope ONE
         aclset = ACLSet(base)
@@ -34,7 +35,7 @@ class TestACLResolver(unittest.TestCase):
         self.resolver.add_acl_set(aclset)
 
         # Check for read, write, create, execute permisions
-        base = self.ldap_base
+        base = "dc=a," + self.ldap_base
         self.assertTrue(self.resolver.get_permissions('tester1',base,'com.gosa.factory','r'),
                 "User is not able to read!")
         self.assertTrue(self.resolver.get_permissions('tester1',base,'com.gosa.factory','w'),
@@ -44,8 +45,17 @@ class TestACLResolver(unittest.TestCase):
         self.assertFalse(self.resolver.get_permissions('tester1',base,'com.gosa.factory','d'),
                 "User is able to delete, this acl was not defined before!")
 
-        # Check for read, write, create, execute permisions
-        base = "dc=a," + self.ldap_base
+        # Check for permissions one level above the location we've created acls for.
+        base = "dc=b,dc=a," + self.ldap_base
+        self.assertFalse(self.resolver.get_permissions('tester1',base,'com.gosa.factory','r'),
+                "The user is able to read, this is wrong!")
+        self.assertFalse(self.resolver.get_permissions('tester1',base,'com.gosa.factory','w'),
+                "The user is able to write, this is wrong!")
+        self.assertFalse(self.resolver.get_permissions('tester1',base,'com.gosa.factory','x'),
+                "The user is able to execute, this is wrong!")
+
+        # Check for permissions one level below the location we've created acls for.
+        base = self.ldap_base
         self.assertFalse(self.resolver.get_permissions('tester1',base,'com.gosa.factory','r'),
                 "The user is able to read, this is wrong!")
         self.assertFalse(self.resolver.get_permissions('tester1',base,'com.gosa.factory','w'),
