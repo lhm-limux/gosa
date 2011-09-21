85aa326e8943901f936be0ad825ef235c30fab9f
Fabian Hickert <fabian.hickert@gonicus.de>
2011-09-21 14:46:08 +0200

    Added list  roles command

--- contrib/acl-admin/acl-admin.py
+++ contrib/acl-admin/acl-admin.py
@@ -5,6 +5,8 @@ import argparse
 from gosa.agent.acl import ACL, ACLSet, ACLRole, ACLRoleEntry, ACLResolver, ACLException
 from gosa.common import Environment
 import sys
+import gettext
+_ = gettext.gettext
 
 #import os
 #import re
@@ -19,7 +21,9 @@ import sys
 
 
 class ACLAdmin(object):
-    def __init__(self):
+    def __init__(self, cfgFile):
+        Environment.noargs = True
+        Environment.config = cfgFile
         self.env = Environment.getInstance()
         self.resolver = ACLResolver()
         self.ldap_base = self.resolver.base
@@ -37,20 +41,20 @@ class ACLAdmin(object):
             return("UNKNOWN")
 
     def printReportHeader(self, string):
-        print("\n")
+        print
         print("-" * len(string))
         print(string)
         print("-" * len(string))
 
-    def listACLs(self):
-        self.printReportHeader("Listing of active GOsa-ng ACLs")
+    def listAcls(self):
+        self.printReportHeader(_("Listing of active GOsa-ng acls"))
         allSets = self.resolver.list_acls()
+        if not len(allSets):
+            print("   ... none")
         for aclset in allSets:
             for acl in aclset:
-
                 print("ID: %i \tBASE: %s \t SCOPE (%s)" % (acl.id, aclset.base, self.idToScopeStr(acl.scope)))
                 print("       \tMEMBER: %s" % (" ".join(acl.members)))
-
                 if acl.uses_role:
                     print("\trefers to role: %s" % acl.role)
                 else:
@@ -60,24 +64,40 @@ class ACLAdmin(object):
                         print("        - %s. %s (%s)  %s" % (cnt, action['topic'], action['acls'], action['options']))
                         cnt += 1
 
+    def listRoles(self):
+        self.printReportHeader(_("Listing of active GOsa-ng roles"))
+        allRoles = self.resolver.list_roles()
+        if not len(allRoles):
+            print("   ... none")
+        for aclrole in allRoles:
+            for acl in aclrole:
+                print("ID: %i \tROLENAME: %s \t SCOPE (%s)" % (acl.id, aclset.name, self.idToScopeStr(acl.scope)))
+                if acl.uses_role:
+                    print("\trefers to role: %s" % acl.role)
+                else:
+                    cnt = 1
+                    print("     \tACTIONS:")
+                    for action in acl.actions:
+                        print("        - %s. %s (%s)  %s" % (cnt, action['topic'], action['acls'], action['options']))
+                        cnt += 1
 def main():
 
     # Define cli-script parameters
     parser = argparse.ArgumentParser(description="Administrate GOsa-ng permissions from the command line.",
             prog="acl-admin")
 
-    parser.add_argument("-l", dest="listAcls", action="store_true", help="Show ACls")
+    parser.add_argument("-c", "--config", default="/etc/gosa/config", dest="cfgFile", help=_("the agent-config file to use"))
+    parser.add_argument("-l", dest="listAcls", action="store_true", help=_("shows a list of all defined acls"))
+    parser.add_argument("-r", dest="listRoles", action="store_true", help=_("shows a list of defined acl-roles"))
 
     # Check if at least 'message' and 'title' are given.
-    print sys.argv
-    args = parser.parse_args()
-
-
-    if args.listAcls:
-        r = ACLAdmin()
-        r.listACLs()
-    else:
-        parser.print_help()
+    my_args = parser.parse_args()
+    if my_args.listAcls:
+        r = ACLAdmin(my_args.cfgFile)
+        r.listAcls()
+    if my_args.listRoles:
+        r = ACLAdmin(my_args.cfgFile)
+        r.listRoles()
 
 if __name__ == '__main__':
     main()
