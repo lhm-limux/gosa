4c246455fb7beb3c9766df8d8abbda73ec7d49d4
Cajus Pollmeier <pollmeier@gonicus.de>
2011-09-30 15:14:34 +0200

    Updated zo client test

--- contrib/zodb/client.py
+++ contrib/zodb/client.py
@@ -1,3 +1,15 @@
+"""
+Prepare:
+
+# easy_install RelStorage
+
+
+From the docs:
+
+ZODB databases can be accessed from multithreaded Python programs. The Storage
+and DB instances can be shared among several threads, as long as individual
+Connection instances are created for each thread.
+"""
 import ZODB.config
 import transaction
 
@@ -5,11 +17,11 @@ db = ZODB.config.databaseFromURL('client.conf')
 conn = db.open()
 root = conn.root()
 
-## Store some things in the root
+# Store some things in the root
 #root['list'] = ['a', 'b', 1.0, 3]
 #root['dict'] = {'a':1, 'b':4}
-#
-## Commit the transaction
+
+# Commit the transaction
 #transaction.commit()
 
 print root['list']
