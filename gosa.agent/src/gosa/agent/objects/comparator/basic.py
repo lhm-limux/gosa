3cdfd411efa2d1d74828122cc479b8e51d799255
Fabian Hickert <fabian.hickert@gonicus.de>
2011-10-04 17:22:26 +0200

    Updated basic comparators to be able to handle lists

--- gosa.agent/src/gosa/agent/objects/comparator/basic.py
+++ gosa.agent/src/gosa/agent/objects/comparator/basic.py
@@ -22,6 +22,8 @@ class Equals(ElementComparator):
         # Check each property value
         cnt = 0
         for item in value:
+
+            # Depending on the ignore-case parameter we do not match upper/lower-case differences.
             if case_ignore:
                 if item.lower() != match.lower():
                     errors.append("Item %s (%s) did not match the given value '%s'! (ignore case)" % (
@@ -37,18 +39,52 @@ class Equals(ElementComparator):
 
 
 class Greater(ElementComparator):
+    """
+    Object property validator which checks if a given property value is
+    greater than a given operand.
+
+    =========== ==================
+    Key         Description
+    =========== ==================
+    match       The value we match againt.
+    =========== ==================
+    """
 
     def __init__(self, obj):
         super(Greater, self).__init__()
 
     def process(self, key, value, match, errors=[]):
-        return value > match
+
+        # All items of value have to match.
+        cnt = 0
+        for item in value:
+            if not (value > match):
+                errors.append("Item %s (%s) is not greater then '%s'!" % (cnt, item, match))
+            cnt += 1
+        return True
 
 
 class Smaller(ElementComparator):
 
+    """
+    Object property validator which checks if a given property value is
+    smaller than a given operand.
+
+    =========== ==================
+    Key         Description
+    =========== ==================
+    match       The value we match againt.
+    =========== ==================
+    """
     def __init__(self, obj):
         super(Smaller, self).__init__()
 
     def process(self, key, value, match, errors=[]):
-        return value < match
+
+        # All items of value have to match.
+        cnt = 0
+        for item in value:
+            if not (value < match):
+                errors.append("Item %s (%s) is not smaller then '%s'!" % (cnt, item, match))
+            cnt += 1
+        return True
