954f4f2c0e1dadf28f90ef220072bfb840b3f57a
Fabian Hickert <fabian.hickert@gonicus.de>
2011-10-04 11:30:57 +0200

    Updated string filters, respect None values

--- gosa.agent/src/gosa/agent/objects/filter/strings.py
+++ gosa.agent/src/gosa/agent/objects/filter/strings.py
@@ -31,11 +31,12 @@ class ConcatString(ElementFilter):
         super(ConcatString, self).__init__(obj)
 
     def process(self, obj, key, valDict, appstr, position):
-        if position == "right":
-            new_val = map(lambda x: x + appstr, valDict[key]['value'] )
-        else:
-            new_val = map(lambda x: appstr + x, valDict[key]['value'] )
-        valDict[key]['value'] = new_val
+        if type(valDict[key]['value'] != None):
+            if position == "right":
+                new_val = map(lambda x: x + appstr, valDict[key]['value'] )
+            else:
+                new_val = map(lambda x: appstr + x, valDict[key]['value'] )
+            valDict[key]['value'] = new_val
         return key, valDict
 
 
@@ -65,7 +66,8 @@ class Replace(ElementFilter):
         super(Replace, self).__init__(obj)
 
     def process(self, obj, key, valDict, regex, replacement):
-        valDict[key]['value'] = map(lambda x: re.sub(regex, str(replacement), x), valDict[key]['value'])
+        if type(valDict[key]['value'] != None):
+            valDict[key]['value'] = map(lambda x: re.sub(regex, str(replacement), x), valDict[key]['value'])
         return key, valDict
 
 
@@ -93,7 +95,8 @@ class DateToString(ElementFilter):
         super(DateToString, self).__init__(obj)
 
     def process(self, obj, key, valDict, fmt="%Y%m%d%H%M%SZ"):
-        valDict[key]['value'] = map(lambda x: x.strftime(fmt), valDict[key]['value'])
+        if type(valDict[key]['value'] != None):
+            valDict[key]['value'] = map(lambda x: x.strftime(fmt), valDict[key]['value'])
         return key, valDict
 
 
@@ -144,7 +147,8 @@ class StringToDate(ElementFilter):
         super(StringToDate, self).__init__(obj)
 
     def process(self, obj, key, valDict, fmt="%Y%m%d%H%M%SZ"):
-        valDict[key]['value'] = map(lambda x: datetime.datetime.strptime(x, fmt).date(), valDict[key]['value'])
+        if type(valDict[key]['value'] != None):
+            valDict[key]['value'] = map(lambda x: datetime.datetime.strptime(x, fmt).date(), valDict[key]['value'])
         return key, valDict
 
 
@@ -172,5 +176,6 @@ class StringToTime(ElementFilter):
         super(StringToTime, self).__init__(obj)
 
     def process(self, obj, key, valDict, fmt="%Y%m%d%H%M%SZ"):
-        valDict[key]['value'] = map(lambda x: datetime.datetime.strptime(x, fmt), valDict[key]['value'])
+        if type(valDict[key]['value'] != None):
+            valDict[key]['value'] = map(lambda x: datetime.datetime.strptime(x, fmt), valDict[key]['value'])
         return key, valDict
